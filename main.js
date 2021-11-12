5f9c7309f5c0d295c68bcd886ccb74f51b01f8f2
wdlnydhcg <MrAlenZhong@163.com>
2021-11-12 15:11:05 +0800

    [ADD] 项目初始化，实现了dialog,文件选择器，菜单，系统托盘，弹出框等功能

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+/*
+ * @Author: MrAlenZhong
+ * @Date: 2021-11-04 11:50:23
+ * @LastEditors: MrAlenZhong
+ * @LastEditTime: 2021-11-12 15:09:25
+ * @Description: 
+ */
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  ipcMain,
+  Menu,
+  Tray,
+} = require("electron");
+  
+    // require("@electron/remote/main").enable(mainWindow.webContents);
 const path = require('path')
 
 function createWindow () {
@@ -8,27 +25,58 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      // 开启node
+      nodeIntegration: true,
+      contextIsolation: false,
+      // 开启remote
+      enableRemoteModule: true,
 
-  // and load the index.html of the app.
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  require("@electron/remote/main").initialize();
+  require("@electron/remote/main").enable(mainWindow.webContents);
+
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.send(
+    "send-msg-to-renderer",
+    "我是主，我主动和你接触"
+  );
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+let tray;
 app.whenReady().then(() => {
   createWindow()
+  //不知道为啥不能用这个icns
+  // const tray = new Tray(path.join("./icon/app.icns")); 
+  tray = new Tray(path.join("./icon/icon@3x.png"));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" },
+  ]);
+  tray.setToolTip("This is my application.");
+  tray.setContextMenu(contextMenu);
+
+  const ret = globalShortcut.register("CommandOrControl+I", () => {
+    console.log("CommanOrControl+I  is pressed");
+  });
+  if(!ret){
+    console.log("register failed");
+  }
+  console.log(globalShortcut.isRegistered("CommanOrControl+I"));
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    // if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    
   })
 })
 
@@ -36,8 +84,20 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
+  globalShortcut.unregisterAll()
   if (process.platform !== 'darwin') app.quit()
 })
+
+
+ipcMain.on("asynchronous-message", (event, arg) => {
+  console.log(arg);
+  event.reply("asynchronous-reply", "主的信息1");
+});
+
+ipcMain.on("synchronous-message", (event, arg) => {
+  console.log(arg);
+  event.returnValue = "主的信息2";
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,6 +1,204 @@
+/*
+ * @Author: MrAlenZhong
+ * @Date: 2021-11-04 11:50:23
+ * @LastEditors: MrAlenZhong
+ * @LastEditTime: 2021-11-09 16:54:57
+ * @Description: 
+ */
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const {ipcRenderer} = require("electron");
+const {
+  BrowserWindow,
+  dialog,
+  globalShortcut,
+  Menu,
+  MenuItem,
+  net
+} = require("@electron/remote");
+
+ 
+function openDialog() {
+  console.log("BrowserWindow", BrowserWindow);
+  console.log("dialog ", dialog);
+   dialog.showOpenDialog({
+     title:'选择你喜欢的文件',
+     buttonLabel:'走你',
+     filters:[
+       {name:"自定义文件",extension:['js','html','json']}
+     ]
+   },result=>{
+     console.log(result);
+   })
+}
+
+function saveDialog() {
+  dialog.showSaveDialog({
+    title:"请选择要保存的文件名",
+    buttonLabel:"保存",
+    filters:[
+      {name:'选择类型',extensions:['js','html']}
+    ]
+  },result => {
+    console.log("result");
+    fs.writeFileSync(result,"保存文件")
+  })
+}
+
+function showMessageDialog() {
+  dialog.showMessageBox({
+    type: "warning",
+    title: "确定吗",
+    message: "真的要删除吗",
+    buttons: ["OK", "cancel"],
+  });
+}
+
+globalShortcut.register("CommandOrControl+G", () => {
+  console.log("您按下了ctrl + G");
+});
+
+// console.log(); 
+ipcRenderer.sendSync("synchronous-message", "渲染的信息1");
+
+ipcRenderer.on("asynchronous-reply", (event, arg) => {
+  console.log("asynchronous-reply"); // prints "pong"
+});
+ipcRenderer.send("asynchronous-message", "渲染的信息2");
+
+ipcRenderer.on("send-msg-to-renderer",(event,arg)=>{
+  console.log("send-msg-to-renderer ", arg);
+});
+
+function openMenu(){
+  const isMac = process.platform === "darwin";
+  const template = [
+    {
+      label: "File111",
+      submenu: [isMac ? { role: "about" } : { role: "about" }],
+    },
+    {
+      label: "File222",
+      submenu: [isMac ? { role: "about" } : { role: "about" }],
+    }
+    // {
+    //   label: "File222",
+    //   submenu: [isMac ? { role: "close" } : { role: "quit" }],
+    // },
+    // ...(isMac
+    //   ? [
+    //       {
+    //         label: "xxxx",
+    //         submenu: [
+    //           { role: "about" },
+    //           { type: "separator" },
+    //           { role: "services" },
+    //           { type: "separator" },
+    //           { role: "hide" },
+    //           { role: "hideOthers" },
+    //           { role: "unhide" },
+    //           { type: "separator" },
+    //           { role: "quit" },
+    //         ],
+    //       },
+    //     ]
+    //   : []),
+    // {
+    //   label: "Filexxx",
+    //   submenu: [isMac ? { role: "close" } : { role: "quit" }],
+    // },
+    // {
+    //   label: "Edit",
+    //   submenu: [
+    //     { role: "undo" },
+    //     { role: "redo" },
+    //     { type: "separator" },
+    //     { role: "cut" },
+    //     { role: "copy" },
+    //     { role: "paste" },
+    //     ...(isMac
+    //       ? [
+    //           { label: "xxxxsss" },
+    //           { role: "pasteAndMatchStyle" },
+    //           { role: "delete" },
+    //           { role: "selectAll" },
+    //           { type: "separator" },
+    //           {
+    //             label: "Speech",
+    //             submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+    //           },
+    //         ]
+    //       : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+    //   ],
+    // },
+    // // { role: 'viewMenu' }
+    // {
+    //   label: "View",
+    //   submenu: [
+    //     { role: "reload" },
+    //     { role: "forceReload" },
+    //     { role: "toggleDevTools" },
+    //     { type: "separator" },
+    //     { role: "resetZoom" },
+    //     { role: "zoomIn" },
+    //     { role: "zoomOut" },
+    //     { type: "separator" },
+    //     { role: "togglefullscreen" },
+    //   ],
+    // },
+    // // { role: 'windowMenu' }
+    // {
+    //   label: "Window",
+    //   submenu: [
+    //     { role: "minimize" },
+    //     { role: "zoom" },
+    //     ...(isMac
+    //       ? [
+    //           { type: "separator" },
+    //           { role: "front" },
+    //           { type: "separator" },
+    //           { role: "window" },
+    //         ]
+    //       : [{ role: "close" }]),
+    //   ],
+    // },
+    // {
+    //   role: "help",
+    //   submenu: [
+    //     {
+    //       label: "Learn More",
+    //       click: async () => {
+    //         const { shell } = require("electron");
+    //         await shell.openExternal("https://electronjs.org");
+    //       },
+    //     },
+    //   ],
+    // },
+  ];
+
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  menu.popup();
+}
+
+function accessBaidu() {
+  // const {net} = require('electron')
+  const request = net.request("https://www.baidu.com");
+  request.on("response", (res) => {
+    console.log(`status code ${res.statusCode}`);
+    console.log(`status code ${JSON.stringify(res.header)}`);
+    res.on("data", (chunk) => {
+      console.log("接收到数据", chunk.toString());
+    });
+    res.on("end", () => {
+      console.log("数据接受完");
+    });
+  });
+  request.end();
+}
